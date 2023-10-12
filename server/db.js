d7369e90964eec9f1c49b069a6e4ae2a573132a2
Ayush <ayushk20@iitk.ac.in>
2023-10-12 17:08:53 +0530

    online postgres-sql updated

--- server/db.js
+++ server/db.js
@@ -1,11 +1,17 @@
 const Pool = require('pg').Pool;
 
-const pool  = new Pool({
-    user: "postgres",
-    password: "prateek",
-    host: "localhost",
-    port: 5432,
-    database: "test",
+const dotenv = require('dotenv')
+dotenv.config({path:__dirname+'\\.env'});
+
+const pool = new Pool({
+    user: process.env.user,
+    password: process.env.password,
+    host: process.env.host,
+    port: process.env.port,
+    database: process.env.database,
+    ssl: {
+        rejectUnauthorized: false
+    }
 })
 
 module.exports = pool;
