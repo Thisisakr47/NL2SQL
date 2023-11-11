9d79062bf21dfd3640b07bad75562f1da19ddfb4
Ayush <ayushk20@iitk.ac.in>
2023-11-11 14:45:04 +0530

    cleaned schema-linking

--- client/src/Components/Details.js
+++ client/src/Components/Details.js
@@ -5,56 +5,25 @@ function Details() {
 
     let jsonString = JSON.stringify(jsonData, null, 2);
     let jsonParse = JSON.parse(jsonString);
-    // const {query, question, sql} = jsonParse 
-    const rows = [];
-
-    const data = [
-      { Name: 'John', Age: 25, City: 'New York' },
-      { Name: 'Jane', Age: 30, City: 'London' },
-    ];
-  
+    const row = [];
+    
     for (let index = 0; index < jsonParse.length; index++) {
-      // rows.push(<p>Query : {jsonParse[index]['links']}</p>);
+      const col = [];
       for (let j = 0; j < jsonParse[index]['links'].length; j++) {
         if (jsonParse[index]['links'][j][2] != null) {
-          rows.push(<p>{jsonParse[index]['links'][j][0]} -> {jsonParse[index]['links'][j][2][1]}</p>);
+          col.push(<div className='d-flex flex-column p-4'><div className='text-center'>{jsonParse[index]['links'][j][0]}</div><div className='text-center'>{jsonParse[index]['links'][j][2][1]}</div></div>);
         }
       }
-      // rows.push(<p>Query : {jsonParse[index]['query']}</p>);
-      rows.push(<p key={index}>Question : {jsonParse[index]['question']}</p>);
-      rows.push(<p key={index}>SQL : {jsonParse[index]['sql']}<br></br><hr></hr></p>);
+      row.push(<div className='d-flex mb-3'>{col}</div>)
+      row.push(<p><br></br>Query : {jsonParse[index]['query']}</p>);
+      row.push(<p key={index}>Question : {jsonParse[index]['question']}</p>);
+      row.push(<p key={index}>Predicted SQL : {jsonParse[index]['sql']}<br></br><hr></hr></p>);
     }  
 
   return (
-    // <div className='d-flex flex-column m-3'>
-    //   <pre>{rows}</pre>
-    // </div>
-    <table border="1">
-      <thead>
-        <tr>
-          {/* Render table headers based on the keys of the first object in the data array */}
-          {Object.keys(data[0]).map((key) => (
-            <th key={key}>{key}</th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {/* Render rows based on the data array */}
-        {data.map((row, rowIndex) => (
-          <tr key={rowIndex}>
-            {/* Render cells based on the key-value pairs in each row */}
-            {Object.entries(row).map(([key, value], colIndex) => (
-              <td
-                key={colIndex}
-                style={{ backgroundColor: value === data[1][key] ? 'green' : 'red' }}
-              >
-                {value}
-              </td>
-            ))}
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <div className='d-flex flex-column m-3'>
+      <pre>{row}</pre>
+    </div>
   );
 }
 
