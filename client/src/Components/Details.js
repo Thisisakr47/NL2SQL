bf8a1a10fef8b1c6dcd03bc618f2849931765a74
Ayush <ayushk20@iitk.ac.in>
2023-11-15 16:52:22 +0530

    added color codes

--- client/src/Components/Details.js
+++ client/src/Components/Details.js
@@ -3,24 +3,56 @@ import jsonData from '../pred_explain.json';
 
 function Details() {
 
-    let jsonString = JSON.stringify(jsonData, null, 2);
-    let jsonParse = JSON.parse(jsonString);
-    const row = [];
-    
-    for (let index = 0; index < jsonParse.length; index++) {
-      const col = [];
-      col.push(<div className='d-flex flex-column p-4'><div className='p-2'><b>Input_Column</b></div><div className='font-weight-bold p-2'><b>LinkedColumn</b></div></div>);
-      for (let j = 0; j < jsonParse[index]['links'].length; j++) {
-        if (jsonParse[index]['links'][j][2] != null) {
-          col.push(<div className='d-flex flex-column p-4'><div className='border border-dark text-center p-2'>{jsonParse[index]['links'][j][0]}</div><div className='border border-dark text-center border-top-0 p-2'>{jsonParse[index]['links'][j][2][1]}</div></div>);
-        }
+
+  function valid(linkedColumnValue, query) {
+    const lowercasedColumn = linkedColumnValue.toLowerCase();
+    const lowercasedQuery = query.toLowerCase();
+
+    const wordsToCheck = lowercasedColumn.split(' ');
+    let count = 0, check = 0;
+
+    for(let index=0; index < wordsToCheck.length; index++) {
+      if (lowercasedQuery.includes(wordsToCheck[index])) {
+        count++;
+        check++;
       }
-      row.push(<div className='d-flex mb-3'>{col}</div>)
-      row.push(<p><b>Question : </b>{jsonParse[index]['question']}</p>);
-      // row.push(<p>TrueValue SQL : {jsonParse[index]['query']}</p>);
-      row.push(<p><b>Predicted SQL : </b>{jsonParse[index]['sql']}</p>);
-      row.push(<hr/>)
-    }  
+      if(wordsToCheck[index] === '[#]') {
+        count = check = 0;
+      }
+    }
+
+    return count === check;
+  }
+
+  let jsonString = JSON.stringify(jsonData, null, 2);
+  let jsonParse = JSON.parse(jsonString);
+  const row = [];
+  
+  for (let index = 0; index < 200; index+=2) {
+    const col = [];
+    col.push(<div className='d-flex flex-column p-4'><div className='p-2'><b>Input_Column</b></div><div className='font-weight-bold p-2'><b>LinkedColumn</b></div></div>);
+    for (let j = 0; j < jsonParse[index]['links'].length; j++) {
+      let bgColor = '#F7665E';
+      if (jsonParse[index]['links'][j][2] != null) {
+          const linkedColumnValue = jsonParse[index]['links'][j][2][1];
+          const query = jsonParse[index]['query'];
+          if (valid(linkedColumnValue, query)) {
+            bgColor = '#93D86C';
+          }
+          col.push(
+            <div className={`d-flex flex-column p-4`}>
+              <div className='border border-dark text-center p-2' style={{ backgroundColor: bgColor }}>{jsonParse[index]['links'][j][0]}</div>
+              <div className='border border-dark text-center border-top-0 p-2' style={{ backgroundColor: bgColor }}>{linkedColumnValue}</div>
+            </div>
+          );
+      }
+    }
+    row.push(<div className='d-flex mb-3'>{col}</div>)
+    row.push(<p><b>Question : </b>{jsonParse[index]['question']}</p>);
+    // row.push(<p><b>TrueValue SQL : </b>{jsonParse[index]['query']}</p>);
+    row.push(<p><b>Predicted SQL : </b>{jsonParse[index]['sql']}</p>);
+    row.push(<hr/>)
+  }  
 
   return (
     <div className='d-flex flex-column m-3'>
